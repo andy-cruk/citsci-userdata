1c6be5eba36dd8e93c960d5b720a00909eb6fe9f
andy-cruk <andy.paterson@cancer.org.uk>
2015-12-09 16:50:21 +0000

    testng ci

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
+    sys.path.append(os.path.join(os.path.dirname(__file__),'env/lib/python2.7/site-packages'))
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "userdata.settings")
 
     from django.core.management import execute_from_command_line
--- userdata/wsgi.py
+++ userdata/wsgi.py
@@ -11,8 +11,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "userdata.settings")
 
 import sys
-sys.path.append(os.path.dirname(__file__))
-sys.path.append(os.path.join(os.path.dirname(__file__),'env/lib/python2.7/site-packages'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+sys.path.append(os.path.join(os.path.dirname(__file__),'../env/lib/python2.7/site-packages'))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
